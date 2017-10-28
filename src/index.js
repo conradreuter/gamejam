ed07fdd2d554522812a878d8cd040ad45cf580ff
Lars Wobus <lars.wobus@web.de>
2017-10-28 20:07:34 +0200

    UPDATE ui with buttons and progressbars

--- src/index.js
+++ src/index.js
@@ -2,10 +2,84 @@ import constants from './constants'
 import GameState from './GameState'
     
 import jQuery from 'jquery';
+import $ from 'jquery';
+import uiButton from './ui/button'
     
 // import * as semantic from '../css/semantic/semantic.js';
 import semantic from 'semantic-ui/dist/semantic.js';
 import 'semantic-ui/dist/semantic.css';
+
+
+/*$('.button')
+  .state({
+    text: {
+      inactive   : 'Follow',
+      active     : 'Followed',
+      deactivate : 'Unfollow'
+    }
+  })
+;*/
+
+window.ui = {}
+
+var UI = window.ui;
+UI.setLives = function(value)
+{
+  $('#lives').text(value);
+}
+UI.setCoins = function(value)
+{
+  $('#coins').text(value);
+}
+UI.setKills = function(value)
+{
+  $('#kills').text(value);
+}
+UI.setTowerPrice = function(towertype, value)
+{
+  $('#'+towertype).text(value);
+}
+UI.setTowerExperience = function(towertype, value)
+{
+  $('#'+towertype)
+    .progress({
+      value    : value,
+      total    : 100,
+      text     : {
+      active: '{value} of {total} done'
+    }
+  });
+}
+
+UI.reset = function()
+{
+  UI.setLives(0);
+  UI.setCoins(0);
+  UI.setKills(0);
+  UI.setTowerPrice("tower_0", 0);
+  UI.setTowerPrice("tower_1", 0);
+  UI.setTowerPrice("tower_2", 0);
+  UI.setTowerPrice("tower_3", 0);
+  UI.setTowerPrice("tower_4", 0);
+  UI.setTowerExperience("progress_0", 0);
+  UI.setTowerExperience("progress_1", 0);
+  UI.setTowerExperience("progress_2", 0);
+  UI.setTowerExperience("progress_3", 0);
+  UI.setTowerExperience("progress_4", 0);
+}
+$('#progress_0').progress().onclick = function(){console.log("hello world");};
+UI.reset();
+
+/*$('.ui.progress')
+    .progress({
+      duration : 10,
+      total    : 100,
+      text     : {
+      active: '{value} of {total} done'
+    }
+  });*/
+
+
 
 window.$constants = constants
 window.$gameState = new GameState()
@@ -15,4 +89,5 @@ window.$game = new Phaser.Game({
   renderer: Phaser.WEBGL,
   state: $gameState,
   width: 800,
+  id: "phaserCanvas"
 })
