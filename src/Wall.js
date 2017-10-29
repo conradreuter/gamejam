aa8a407c01d6e6dbbe2ec0a1462c029cacd0a19e
Conrad Reuter <conrad.reuter@enavo.com>
2017-10-29 13:23:49 +0100

    Fix mouse reenter bug

--- src/Wall.js
+++ src/Wall.js
@@ -9,10 +9,26 @@ export default class Wall extends Entity {
     Wall.layer = $game.add.group()
     $game.load.image('wall', sprite)
     $game.load.image('marker', markerSprite)
+
+    $game.input.mouse.mouseOutCallback = Wall.unhoverLastHovered
+    $game.input.mouse.mouseOverCallback = Wall.restoreLastHovered
+  }
+
+  static unhoverLastHovered() {
+    if (!Wall.lastHovered) return
+    Wall.lastHovered.unhover()
+  }
+
+  static restoreLastHovered() {
+    $game.physics.arcade.getObjectsUnderPointer($game.input.activePointer, Wall.layer, (pointer, wallSprite) => {
+      const wall = wallSprite.data
+      wall.hover()
+    })
   }
 
   create() {
     this.sprite = Wall.layer.create(this.x, this.y, 'wall')
+    this.sprite.data = this
     this.markerSprite = Wall.layer.create(this.x, this.y, 'marker')
     $game.physics.arcade.enable(this.sprite)
     this.sprite.body.immovable = true
@@ -20,10 +36,12 @@ export default class Wall extends Entity {
     this.sprite.events.onInputDown.add(this.select, this)
     this.sprite.events.onInputOut.add(this.unhover, this)
     this.sprite.events.onInputOver.add(this.hover, this)
+    this.sprite.input.useHandCursor = true
   }
 
   hover() {
     this.hovered = true
+    Wall.lastHovered = this
   }
 
   unhover() {
