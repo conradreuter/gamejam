d08cdb938a330c3277f3d4966cea87d4c71d6d1e
Conrad Reuter <conrad.reuter@enavo.com>
2017-10-29 15:59:00 +0100

    Gameover sound

--- src/GameOver.js
+++ src/GameOver.js
@@ -1,6 +1,10 @@
+import sound from '../assets/audio/gameover.wav'
+
 export default {
 
   preload() {
+    $game.load.audio('gameover', sound)
+
     const text = $game.add.text(0, 0, 'GAME\nOVER', {
       boundsAlignH: 'center',
       boundsAlignV: 'middle',
@@ -9,5 +13,9 @@ export default {
     })
     text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2)
     text.setTextBounds(0, 0, $game.width, $game.height)
-  }
+  },
+
+  create() {
+    $game.add.audio('gameover').play()
+  },
 }
